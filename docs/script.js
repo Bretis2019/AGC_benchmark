b1f6d3a4d3e5924aa39a3703a0afa4a9186d3d4f
Bretis2019 <mosbahiwalid10@gmail.com>
2023-03-03 18:33:42 +0100

    update

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,10 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const fileInput = document.querySelector('#script-file');
-  const scriptFile = fileInput.files[0];
-  const formData = new FormData();
-  formData.append('script_file', scriptFile);
-  const response = await fetch('https://bretis2019.github.io/AGC_benchmark/app.py', {
-    method: 'POST',
-    body: formData
-  });
-  const score = await response.text();
-  const scoreContainer = document.querySelector('#score-container');
-  scoreContainer.textContent = `Your computer is ${score} times more powerful than the AGC!`;
-});
+function calculateScore() {
+  const AGC_FLOPS = 1000000; // 1 MHz
+  const userFlopsScore = parseInt(document.getElementById("flops-score").value);
+  
+  if (!isNaN(userFlopsScore)) {
+    const score = Math.round(userFlopsScore / AGC_FLOPS);
+    const scoreContainer = document.getElementById("score-container");
+    scoreContainer.innerHTML = `Your computer is approximately <strong>${score} times</strong> more powerful than the Apollo Guidance Computer.`;
+  }
+}
