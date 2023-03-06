19333f43a51d98f1fb45683f93f3bdba062a7ae8
Bretis2019 <mosbahiwalid10@gmail.com>
2023-03-06 21:03:46 +0100

    updated create team

--- docs/script.js
+++ docs/script.js
@@ -1,82 +1,94 @@
-// Get the teams from the JSON file
-async function getTeams() {
-  const response = await fetch('teams.json');
-  const teams = await response.json();
-  teams.forEach(team => {
-    createTeamCard(team);
-  });
+const teamsContainer = document.getElementById('teams-container');
+
+// Render teams from the teams.json file
+async function renderTeams() {
+  try {
+    const response = await fetch('teams.json');
+    const teams = await response.json();
+    teams.forEach((team) => {
+      renderTeam(team);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-// Create a new team card
-function createTeamCard(team) {
-  const teamsContainer = document.getElementById('teams-container');
-
+// Render a single team
+function renderTeam(team) {
   const teamCard = document.createElement('div');
   teamCard.classList.add('team-card');
 
-  const teamName = document.createElement('h3');
-  teamName.textContent = team.name;
-  teamCard.appendChild(teamName);
+  const nameElement = document.createElement('h2');
+  nameElement.textContent = team.name;
+  teamCard.appendChild(nameElement);
 
-  const teamInfo = document.createElement('p');
-  teamInfo.textContent = `${team.players} players · ${team.location}`;
-  teamCard.appendChild(teamInfo);
+  const playersElement = document.createElement('p');
+  playersElement.textContent = `Number of players: ${team.players}`;
+  teamCard.appendChild(playersElement);
 
-  const contactInfo = document.createElement('p');
-  contactInfo.textContent = team.contact;
-  teamCard.appendChild(contactInfo);
+  const locationElement = document.createElement('p');
+  locationElement.textContent = `Location: ${team.location}`;
+  teamCard.appendChild(locationElement);
+
+  const contactButton = document.createElement('button');
+  contactButton.textContent = 'Contact Team';
+  contactButton.addEventListener('click', async () => {
+    const contactInfo = await getContactInfo(team.contact);
+    const contactElement = document.createElement('p');
+    contactElement.textContent = `Email: ${contactInfo.email}`;
+    teamCard.appendChild(contactElement);
+    contactButton.remove();
+  });
+  teamCard.appendChild(contactButton);
 
   teamsContainer.appendChild(teamCard);
 }
 
-// Function to add a new team to the JSON file and display its card
+// Get contact info for a team
+async function getContactInfo(contactUrl) {
+  try {
+    const response = await fetch(contactUrl);
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return { email: 'Unavailable' };
+  }
+}
+
+// Handle form submission to add a new team
 async function addTeam(event) {
   event.preventDefault();
-  const name = addTeamForm.elements['name-input'].value;
-  const players = addTeamForm.elements['players-input'].value;
-  const location = addTeamForm.elements['location-input'].value;
-  const contact = addTeamForm.elements['contact-input'].value;
 
-  const newTeam = { name, players: parseInt(players), location, contact };
+  const name = document.getElementById('name-input').value;
+  const players = document.getElementById('players-input').value;
+  const location = document.getElementById('location-input').value;
+  const contact = document.getElementById('contact-input').value;
+
+  const newTeam = {
+    name,
+    players: parseInt(players),
+    location,
+    contact
+  };
 
   try {
-    // Send the new team data to Formspree
-    const response = await fetch('https://formspree.io/f/mbjeyegq', {
+    const response = await fetch('/.netlify/functions/addTeam', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
       body: JSON.stringify(newTeam)
     });
-    const team = await response.json();
 
-    // Update the local teams array and the teams.json file
-    const teamsResponse = await fetch('teams.json');
-    let teams = await teamsResponse.json();
-    teams.push(team);
-    const updateResponse = await fetch('teams.json', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(teams)
-    });
-
-    // If the update was successful, create the new team card
-    if (updateResponse.ok) {
-      createTeamCard(team);
-      addTeamForm.reset();
+    if (response.ok) {
+      console.log('New team added successfully.');
+      renderTeam(newTeam);
+      document.getElementById('add-team-form').reset();
     } else {
-      console.error('Failed to update teams.json');
+      console.error('Failed to add new team.');
     }
   } catch (error) {
     console.error(error);
   }
 }
 
-// Call the getTeams function to display the existing teams
-getTeams();
-
-// Add an event listener to the "Add Team" form
-const addTeamForm = document.getElementById('add-team-form');
-addTeamForm.addEventListener('submit', addTeam);
+// Initialize the app
+renderTeams();
+document.getElementById('add-team-form').addEventListener('submit', addTeam);
