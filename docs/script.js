aba5df7eb3de1c6a086599df5ea059d76236fe89
Bretis2019 <mosbahiwalid10@gmail.com>
2023-03-06 20:05:12 +0100

    new website

--- docs/script.js
+++ docs/script.js
@@ -1,10 +1,33 @@
-function calculateScore() {
-  const AGC_FLOPS = 1000000; // 1 MHz
-  const userFlopsScore = parseInt(document.getElementById("flops-score").value);
-  
-  if (!isNaN(userFlopsScore)) {
-    const score = Math.round(userFlopsScore / AGC_FLOPS);
-    const scoreContainer = document.getElementById("score-container");
-    scoreContainer.innerHTML = `Your computer is approximately <strong>${score} times</strong> more powerful than the Apollo Guidance Computer.`;
-  }
-}
+const teamsContainer = document.getElementById('teams-container');
+
+fetch('teams.json')
+  .then(response => response.json())
+  .then(teams => {
+    teams.forEach((team) => {
+      const teamCard = document.createElement('div');
+      teamCard.classList.add('team-card');
+
+  const nameElement = document.createElement('h2');
+  nameElement.textContent = team.name;
+  teamCard.appendChild(nameElement);
+
+  const playersElement = document.createElement('p');
+  playersElement.textContent = `Number of players: ${team.players}`;
+  teamCard.appendChild(playersElement);
+
+  const locationElement = document.createElement('p');
+  locationElement.textContent = `Location: ${team.location}`;
+  teamCard.appendChild(locationElement);
+
+  const contactButton = document.createElement('button');
+  contactButton.textContent = 'Contact Team';
+  contactButton.addEventListener('click', () => {
+    const contactInfo = document.createElement('p');
+    contactInfo.textContent = `Email: ${team.contact}`;
+    teamCard.appendChild(contactInfo);
+    contactButton.remove();
+  });
+  teamCard.appendChild(contactButton);
+
+  teamsContainer.appendChild(teamCard);
+});
