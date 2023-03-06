4df363b500c0c859a25fca9589eadfb87ca68904
Bretis2019 <mosbahiwalid10@gmail.com>
2023-03-06 20:10:19 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,33 +1,37 @@
 const teamsContainer = document.getElementById('teams-container');
 
 fetch('teams.json')
-  .then(response => response.json())
-  .then(teams => {
+  .then((response) => response.json())
+  .then((teams) => {
     teams.forEach((team) => {
       const teamCard = document.createElement('div');
       teamCard.classList.add('team-card');
 
-  const nameElement = document.createElement('h2');
-  nameElement.textContent = team.name;
-  teamCard.appendChild(nameElement);
+      const nameElement = document.createElement('h2');
+      nameElement.textContent = team.name;
+      teamCard.appendChild(nameElement);
 
-  const playersElement = document.createElement('p');
-  playersElement.textContent = `Number of players: ${team.players}`;
-  teamCard.appendChild(playersElement);
+      const playersElement = document.createElement('p');
+      playersElement.textContent = `Number of players: ${team.players}`;
+      teamCard.appendChild(playersElement);
 
-  const locationElement = document.createElement('p');
-  locationElement.textContent = `Location: ${team.location}`;
-  teamCard.appendChild(locationElement);
+      const locationElement = document.createElement('p');
+      locationElement.textContent = `Location: ${team.location}`;
+      teamCard.appendChild(locationElement);
 
-  const contactButton = document.createElement('button');
-  contactButton.textContent = 'Contact Team';
-  contactButton.addEventListener('click', () => {
-    const contactInfo = document.createElement('p');
-    contactInfo.textContent = `Email: ${team.contact}`;
-    teamCard.appendChild(contactInfo);
-    contactButton.remove();
+      const contactButton = document.createElement('button');
+      contactButton.textContent = 'Contact Team';
+      contactButton.addEventListener('click', () => {
+        const contactInfo = document.createElement('p');
+        contactInfo.textContent = `Email: ${team.contact}`;
+        teamCard.appendChild(contactInfo);
+        contactButton.remove();
+      });
+      teamCard.appendChild(contactButton);
+
+      teamsContainer.appendChild(teamCard);
+    });
+  })
+  .catch((error) => {
+    console.error(`Error fetching teams: ${error.message}`);
   });
-  teamCard.appendChild(contactButton);
-
-  teamsContainer.appendChild(teamCard);
-});
